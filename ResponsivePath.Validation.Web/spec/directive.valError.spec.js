adbe63f520407608a7a36aa3f357053505787bdc
Matt DeKrey <mattdekrey@gmail.com>
2014-07-06 21:22:48 -0400

    Add tests for valError and valmsgFor.

--- ResponsivePath.Validation.Web/spec/directive.valError.spec.js
+++ ResponsivePath.Validation.Web/spec/directive.valError.spec.js
@@ -1,1 +1,62 @@
 ﻿
+describe('Directive "valError"', function () {
+    var scope;
+
+    beforeEach(module('ng', 'unobtrusive.validation'));
+
+    beforeEach(inject(function ($rootScope) {
+        scope = $rootScope.$new();
+    }));
+
+    it('adds the error class when there are errors.', function () {
+        // assign
+        var elem;
+        var dotNetName = 'SomeDotNetProperty';
+        var template = '<form data-val-bind-messages="validationMessages"><div data-val-error="' + dotNetName + '"></div></form>'
+        inject(function ($compile, validation) {
+            var form = $compile(template)(scope);
+            elem = form.find('div');
+        });
+
+        scope.$digest();
+
+        // act
+        scope.$apply(function () {
+            scope.validationMessages = [
+                { memberName: dotNetName, text: 'This is a test' },
+                { memberName: dotNetName, text: 'This is another <span>test</span>' }
+            ];
+        });
+
+        // assert
+        inject(function (validation) {
+            expect(elem.hasClass('error')).toBe(true);
+        });
+    });
+
+    it('removes the error class when there are no errors for the given name.', function () {
+        // assign
+        var elem;
+        var dotNetName = 'SomeDotNetProperty';
+        var template = '<form data-val-bind-messages="validationMessages"><div class="error" data-val-error="' + dotNetName + 'Other"></div></form>'
+        inject(function ($compile, validation) {
+            var form = $compile(template)(scope);
+            elem = form.find('div');
+        });
+
+        scope.$digest();
+
+        // act
+        scope.$apply(function () {
+            scope.validationMessages = [
+                { memberName: dotNetName, text: 'This is a test' },
+                { memberName: dotNetName, text: 'This is another <span>test</span>' }
+            ];
+        });
+
+        // assert
+        inject(function (validation) {
+            expect(elem.hasClass('error')).toBe(false);
+        });
+    });
+});
--- ResponsivePath.Validation.Web/spec/directive.valmsgFor.spec.js
+++ ResponsivePath.Validation.Web/spec/directive.valmsgFor.spec.js
@@ -1,1 +1,66 @@
 ﻿
+describe('Directive "valmsgFor"', function () {
+    var scope;
+
+    beforeEach(module('ng', 'unobtrusive.validation'));
+
+    beforeEach(inject(function ($rootScope) {
+        scope = $rootScope.$new();
+    }));
+
+    it('lists out the validation messages, when there are any.', function () {
+        // assign
+        var elem;
+        var dotNetName = 'SomeDotNetProperty';
+        var template = '<form data-val-bind-messages="validationMessages"><span data-valmsg-for="' + dotNetName + '"></span></form>'
+        inject(function ($compile, validation) {
+            var form = $compile(template)(scope);
+            elem = form.find('span');
+        });
+
+        scope.$digest();
+
+        // act
+        scope.$apply(function () {
+            scope.validationMessages = [
+                { memberName: dotNetName, text: 'This is a test' },
+                { memberName: dotNetName, text: 'This is another <span>test</span>' }
+            ];
+        });
+
+        // assert
+        inject(function (validation) {
+            var content = elem.html();
+            expect(content.indexOf('This is a test')).toBeGreaterThan(-1);
+            expect(content.indexOf('This is another <span>test</span>')).toBeGreaterThan(-1);
+        });
+    });
+
+    it('hides the validation messages, when there are none.', function () {
+        // assign
+        var elem;
+        var dotNetName = 'SomeDotNetProperty';
+        var template = '<form data-val-bind-messages="validationMessages"><span class="error" data-valmsg-for="' + dotNetName + 'Other"></span></form>'
+        inject(function ($compile, validation) {
+            var form = $compile(template)(scope);
+            elem = form.find('span');
+        });
+
+        scope.$digest();
+
+        // act
+        scope.$apply(function () {
+            scope.validationMessages = [
+                { memberName: dotNetName, text: 'This is a test' },
+                { memberName: dotNetName, text: 'This is another <span>test</span>' }
+            ];
+        });
+
+        // assert
+        inject(function (validation) {
+            var content = elem.html();
+            expect(content.indexOf('This is a test')).toEqual(-1);
+            expect(content.indexOf('This is another <span>test</span>')).toEqual(-1);
+        });
+    });
+});
