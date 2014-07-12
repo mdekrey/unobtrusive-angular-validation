315f16e0223dc2b6eceb4d76782a2a15e1591388
Matt DeKrey <mattdekrey@gmail.com>
2014-07-11 20:38:10 -0400

    Prepare for NuGet release.

--- ResponsivePath.Validation.Web/Global.asax.cs
+++ ResponsivePath.Validation.Web/Global.asax.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
 using System.Web.Security;
 using System.Web.SessionState;
@@ -20,7 +21,21 @@ namespace ResponsivePath.Validation.Web
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+
+            BundleTable.Bundles.Add(new ScriptBundle("~/bundles/angularValidation")
+                .Include("~/Scripts/angular.unobtrusive.validation.js")
+            );
+
         }
 
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+            if (this.Context.Request.AppRelativeCurrentExecutionFilePath == "~/bundles/angularValidation")
+            {
+                var temp = BundleTable.Bundles.GetBundleFor("~/bundles/angularValidation").GenerateBundleResponse(new BundleContext(new HttpContextWrapper(this.Context), BundleTable.Bundles, string.Empty)).Content;
+                System.IO.File.WriteAllText(Context.Server.MapPath("~/Scripts/angular.unobtrusive.validation.min.js"), temp);
+            }
+
+        }
     }
 }
--- ResponsivePath.Validation.Web/Scripts/_references.js
+++ ResponsivePath.Validation.Web/Scripts/_references.js
@@ -3,6 +3,6 @@
 /// <reference path="//cdnjs.cloudflare.com/ajax/libs/jasmine/2.0.0/jasmine.js"/>
 /// <reference path="//cdnjs.cloudflare.com/ajax/libs/jasmine/2.0.0/jasmine-html.js"/>
 /// <reference path="//cdnjs.cloudflare.com/ajax/libs/jasmine/2.0.0/boot.js"/>
-/// <reference path="/Scripts/validation.js" />
+/// <reference path="/Scripts/angular.unobtrusive.validation.js" />
 
 /// <reference path="../Libraries/_intellisense.js" />
