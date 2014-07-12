5c1cc43c4ad678eb98aa5f290c2559ce332ad391
Matt DeKrey <mattdekrey@gmail.com>
2014-07-11 21:13:57 -0400

    Include package creation process.

---
+++ package.bat
@@ -0,0 +1,3 @@
+%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\msbuild ResponsivePath.Validation\ResponsivePath.Validation.csproj /p:Configuration=Release
+.nuget\NuGet pack ResponsivePath.Validation\ResponsivePath.Validation.csproj -Prop Configuration=Release
+.nuget\NuGet pack ResponsivePath.Validation.UnobtrusiveAngular.nuspec
