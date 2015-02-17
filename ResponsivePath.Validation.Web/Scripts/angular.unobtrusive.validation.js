ecf342f6d54d8bf0bcfcd25104f77e4b857fac75
unknown <jmyers@DEV-VDI-5.HQ.Nerium.com>
2015-02-17 16:21:33 -0600

    Changed directive templates to use templateUrl and $templateCache instead of inline templates.

--- ResponsivePath.Validation.Web/Scripts/angular.unobtrusive.validation.js
+++ ResponsivePath.Validation.Web/Scripts/angular.unobtrusive.validation.js
@@ -287,12 +287,7 @@
         return {
             restrict: 'A',
             scope: {},
-            template: '<div class="alert alert-error" ng-if="started" ng-show="validationSummary.length">' +
-                '   <ul>' +
-                '       <li data-ng-repeat="err in validationSummary" data-ng-bind-html="err"></span>' +
-                '   </ul>' +
-                '</div>' +
-                '<div class="alert alert-error" ng-transclude ng-if="!started"></div>',
+            templateUrl: 'templates/angular-unobtrusive-validation/valmsgSummary.html',
             transclude: true,
             link: function (scope, element) {
                 scope.started = false;
@@ -382,7 +377,7 @@
             scope: {
                 valmsgFor: '@'
             },
-            template: '<span for="{{valmsgFor}}" data-ng-repeat="err in messages" generated="true" data-ng-bind-html="err"></span><span ng-transclude ng-if="!messages"></span>',
+            templateUrl: 'templates/angular-unobtrusive-validation/valmsgFor.html',
             transclude: true,
             link: function (scope, element) {
                 scope.validationSummary = [];
@@ -569,5 +564,18 @@
             });
             return true;
         }, ['validation', '$http', '$q']);
-    }]);
+    }])
+
+    .run(function ($templateCache) {
+        $templateCache.put("templates/angular-unobtrusive-validation/valmsgSummary.html",
+            '<div class="alert alert-error" ng-if="started" ng-show="validationSummary.length">' +
+            '   <ul>' +
+            '       <li data-ng-repeat="err in validationSummary" data-ng-bind-html="err"></span>' +
+            '   </ul>' +
+            '</div>' +
+            '<div class="alert alert-error" ng-transclude ng-if="!started"></div>'
+        );
+
+        $templateCache.put("templates/angular-unobtrusive-validation/valmsgFor.html", '<span for="{{valmsgFor}}" data-ng-repeat="err in messages" generated="true" data-ng-bind-html="err"></span><span ng-transclude ng-if="!messages"></span>');
+    });
 })();
